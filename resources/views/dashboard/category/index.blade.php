1068f8060ed8ea0ad0d1379a6047b6e3fe0995d3
OmidProgramm <omidvarisl@gmx.de>
2026-07-22 14:58:15 +0200

    feat: create structure category

---
+++ resources/views/dashboard/category/index.blade.php
@@ -0,0 +1,47 @@
+@extends('dashboard.layouts.master')
+@section('css')
+ <link rel="stylesheet" href="{{ asset('admin/assets/css/sliderIndex.css') }}">
+@endsection
+
+@section('content')
+    @php
+        use Illuminate\Support\Str;
+    @endphp
+     @if(session('createCategory'))
+        <p class="createSession">{{session('createCategory')}}</p>
+    @endif
+<div class="sliderDak">
+    <table class="showSlider">
+        <tr>
+            <th>Title</th>
+            <th>Image</th>
+            <th>Delete</th>
+            <th>Update</th>
+        </tr>
+        @forelse ($category as $item)
+            <tr>
+                <td>{{$item->title}}</td>
+                <td><img class="imageSlider" src="{{asset('images/category/'.$item->image)}}"></td>
+                <td>
+                    <form action="{{route('category.destroy',['id'=>$item->id])}}" method="POST">
+                        @csrf
+                        @method('delete')
+                        <input type="submit" value="delete">
+                    </form>
+                </td>
+                <td>
+                    <form action="{{route('category.edit',['id'=>$item->id])}}" method="GET">
+                        <input type="submit" value="edit" class="edit">
+                    </form>
+                </td>
+            </tr>
+        @empty
+            <tr>
+                <td colspan="4">No Data</td>
+            </tr>
+        @endforelse
+    </table>
+     {{$category->links()}} 
+     <div class="createSlider"><a href="{{route('category.create')}}">Create-Category</a></div>
+</div>
+@endsection
--- resources/views/dashboard/layouts/master.blade.php
+++ resources/views/dashboard/layouts/master.blade.php
@@ -21,6 +21,7 @@
                     <li><a class="backHome" href="{{route('slider.index')}}">slider</a></li>
                     <li><a class="backHome" href="{{route('about.index')}}">about</a></li>
                     <li><a class="backHome" href="{{route('team.index')}}">team</a></li>
+                    <li><a class="backHome" href="{{route('category.index')}}">category</a></li>
                     <li><a class="backHome" href="{{route('show-website')}}" target="_blank">Show-Website</a></li>
                     <li>
                         {{-- <form action="{{route('logout')}}" method="POST" class="form-not">
